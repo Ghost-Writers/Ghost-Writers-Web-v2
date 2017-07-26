a6e8a641e1cde6c052c11861865b93f84fc5c630
Sho Nguyen <shonozaki@Shos-MacBook-Pro.local>
2017-07-25 19:48:33 -0700

    passing coords to in app browser

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -53,67 +53,68 @@ export class AboutPage implements OnInit {
   launchSite() {
     alert('launching site')
 
-    if (this.platform.is('cordova')) {
-      var browserRef = this.iab
-        .create(`http://createpage.herokuapp.com/?userid=${this.userInfo.user._id}`, "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
-
-      // const exitSubscription: Subscription = browserRef.on("exit").subscribe((event) => {
-      //   console.error("The Facebook sign in flow was canceled");
-      // });
-
-      browserRef.on("loadstart").subscribe((event) => {
-
-        // alert('loadstart event alert success!!!')
-        let localStorageQuery = 'localStorage.setItem("user_id", ")' + this.userInfo.user._id + '")';
-        //  win.executeScript({ code: "localStorage.setItem( 'name', '' );" });
-        browserRef.executeScript({ code: "localStorage.setItem( 'namettt', '' );" }).then(res => {
-          console.log('set')
-          console.log(res)
-          // alert(JSON.stringify(res))
-          alert('after setting test-params to local storage')
-        });
-
-          browserRef.executeScript({ code: "document.getElementById('test-el').value = 123456" }).then(res => {
-          console.log('set')
-          console.log(res)
-          // alert(JSON.stringify(res))
-          alert('after setting test-params to local storage')
-        });
-
-        browserRef.executeScript({ code: "alert(window.localStorage.getItem('name'))" }).then(res => {
-          console.log('after executing script')
-          console.log(res)
-          // alert(JSON.stringify(res))
-          alert('after setting localStorage')
-        }).catch(err => {
-          alert('error happened')
-          alert(JSON.stringify(err))
-        });
-
-        // 'localStorage.setItem("user-from-mobile")'
-
-        // browserRef.executeScript({file: 'local.storage.script.js'}).then(res => {
-        //   console.log('2: after executing script')
-        //   console.log(res)
-        //   alert(JSON.stringify(res))
-        //   alert('2: after executing script info msg')
-        // });
-        browserRef.executeScript({ code: JSON.stringify(localStorage.setItem('test-key', this.userInfo.user._id)) })
-      });
-    } else {
-      console.error("loadstart events are not being fired in browser.");
-      alert('error in loadstart')
-    }
-
-
-
     this.geolocation.getCurrentPosition().then(
       (resp) => {
         this.currLat = resp.coords.latitude;
         this.currLong = resp.coords.longitude;
+        alert(this.currLat + ':' + this.currLong )
+        if (this.platform.is('cordova')) {
+          var browserRef = this.iab
+            .create('http://createpage.herokuapp.com/?userid=' + localStorage.id + '&currlat=' + this.currLat + '&currlong=' + this.currLong, "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+
+          // const exitSubscription: Subscription = browserRef.on("exit").subscribe((event) => {
+          //   console.error("The Facebook sign in flow was canceled");
+          // });
+
+          browserRef.on("loadstart").subscribe((event) => {
+
+            // alert('loadstart event alert success!!!')
+            let localStorageQuery = 'localStorage.setItem("user_id", ")' + this.userInfo.user._id + '")';
+            //  win.executeScript({ code: "localStorage.setItem( 'name', '' );" });
+            browserRef.executeScript({ code: "localStorage.setItem( 'namettt', '' );" }).then(res => {
+              console.log('set')
+              console.log(res)
+              // alert(JSON.stringify(res))
+              alert('after setting test-params to local storage')
+            });
+
+              browserRef.executeScript({ code: "document.getElementById('test-el').value = 123456" }).then(res => {
+              console.log('set')
+              console.log(res)
+              // alert(JSON.stringify(res))
+              alert('after setting test-params to local storage')
+            });
+
+            browserRef.executeScript({ code: "alert(window.localStorage.getItem('name'))" }).then(res => {
+              console.log('after executing script')
+              console.log(res)
+              // alert(JSON.stringify(res))
+              alert('after setting localStorage')
+            }).catch(err => {
+              alert('error happened')
+              alert(JSON.stringify(err))
+            });
+
+            // 'localStorage.setItem("user-from-mobile")'
+
+            // browserRef.executeScript({file: 'local.storage.script.js'}).then(res => {
+            //   console.log('2: after executing script')
+            //   console.log(res)
+            //   alert(JSON.stringify(res))
+            //   alert('2: after executing script info msg')
+            // });
+            browserRef.executeScript({ code: JSON.stringify(localStorage.setItem('test-key', this.userInfo.user._id)) })
+          });
+        } else {
+          console.error("loadstart events are not being fired in browser.");
+          alert('error in loadstart')
+        }
       }
     )
+
+
+
     // alert('in launch site')
-    let iabRef = this.iab.create('http://createpage.herokuapp.com/', '_blank')
+    // let iabRef = this.iab.create('http://createpage.herokuapp.com/', '_blank')
   }
 }
