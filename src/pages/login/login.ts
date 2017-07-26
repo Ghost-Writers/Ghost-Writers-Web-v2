7bd4aa692b69a5f99fbabfd6dcdf6b3036298766
Jay Hongvarivatana <jayyhong@gmail.com>
2017-07-26 10:18:19 -0700

    bugfix login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -44,7 +44,7 @@ export class LoginPage {
     )
       .subscribe(
       user => this.userPost = user,
-      error => console.log('error boi'),
+      error => swal('Login Failed', 'Please try again', 'error'),
       () => {
         if (this.userPost === null) {
           swal('Login Failed', 'Please try again', 'error')
@@ -52,14 +52,14 @@ export class LoginPage {
           swal('Login Failed', 'Please try again', 'error')
         } else {
           this.navCtrl.setRoot(TabsPage, this.userPost)
-          swal({
-            title: 'Welcome',
-            text: 'Create custom underground ghost art in as little as three steps. Make a custom marker by taking a picture of any painting or unique wall. Upload your art onto the marker. View art with ghost vision.',
-            imageUrl: 'http://weburbanist.com/wp-content/uploads/2016/10/graffiti-revealed-mural-644x369.jpg',
-            imageWidth: 400,
-            imageHeight: 200,
-            animation: false
-          })
+          // swal({
+          //   title: 'Welcome',
+          //   text: 'Create custom underground ghost art in as little as three steps. Make a custom marker by taking a picture of any painting or unique wall. Upload your art onto the marker. View art with ghost vision.',
+          //   imageUrl: 'http://weburbanist.com/wp-content/uploads/2016/10/graffiti-revealed-mural-644x369.jpg',
+          //   imageWidth: 400,
+          //   imageHeight: 200,
+          //   animation: false
+          // })
         }
       }
       )
